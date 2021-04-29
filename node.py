b181d6e572798756007ab96ebd04dc7e9a2e49c3
nemag100 <jszwed23@gmail.com>
2021-04-29 18:32:55 +0200

    Fix and reformat
    From now on, we follow pep8...

--- node.py
+++ node.py
@@ -1,12 +1,13 @@
 class Node(object):
     """Used for storing a representation of bayesian network node"""
+
     def __init__(self, name):
         self.name = name
         self.parents = []
         self.children = []
         self.probabilities = {}
 
-    def __str__(self) # for easier debugging and visualisation
+    def __str__(self):  # for easier debugging and visualisation
         return self.name
 
     def get_probability(events):
