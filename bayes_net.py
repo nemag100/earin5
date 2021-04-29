eddcbf15df6e890e571e8dd8d17f76647d4ed63e
nemag100 <jszwed23@gmail.com>
2021-04-29 18:20:56 +0200

    Initial commit

---
+++ bayes_net.py
@@ -0,0 +1,14 @@
+class BayesNet(object):
+    """Used for storing a bayesian network representation."""
+    def __init__(self):
+        self.nodes = []
+
+    def mcmc(evidence={}, query=[]):
+        """Returns probability estimates for each query,
+        based on provided evidence."""
+        answer = ''
+        for q in query:
+            for n in nodes:
+                pass
+        return answer
+
---
+++ node.py
@@ -0,0 +1,14 @@
+class Node(object):
+    """Used for storing a representation of bayesian network node"""
+    def __init__(self, name):
+        self.name = name
+        self.parents = []
+        self.children = []
+        self.probabilities = {}
+
+    def __str__(self) # for easier debugging and visualisation
+        return self.name
+
+    def get_probability(events):
+        """Returns probabilities of given events chain."""
+        return self.probabilities[events]
