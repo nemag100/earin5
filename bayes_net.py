61a5ebdecd4cbfc6013d5148773b995f39e8b150
nemag100 <jszwed23@gmail.com>
2021-05-06 16:17:57 +0200

    Update bayes_net.py

--- bayes_net.py
+++ bayes_net.py
@@ -1,13 +1,13 @@
-class BayesNet(object):
+from utils import load_json
+
+class BayesNet():
     """Used for storing a bayesian network representation."""
 
     def __init__(self):
         self.nodes = []
 
-    def check_cycles(self):
-        """Evaluates to True if the bayesian network graph
-        contains cycles, otherwise evaluates to False"""
-        pass
+    def load(self, file):
+        self.nodes = load_json(file)
 
     def mcmc(self, evidence={}, query=[]):
         """Returns probability estimates for each query,
@@ -17,3 +17,11 @@ class BayesNet(object):
             for n in nodes:
                 pass
         return answer
+
+    def markov_blanket(self):
+        pass
+
+    def check_cycles(self):
+        """Evaluates to True if the bayesian network graph
+        contains cycles, otherwise evaluates to False"""
+        pass
