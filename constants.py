cf0e745b84644ad94196f59e222bd3a7990eb182
nemag100 <jszwed23@gmail.com>
2021-05-06 16:18:02 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""This module stores utilitary constants, mostly those prepared
+according to EARIN Exercise 5 requirements."""
+
+NODES = 'nodes'
+RELATIONS = 'relations'
+PARENTS = 'parents'
+PROBABILITIES = 'probabilities'
+
+REQUIRED_KEYS = [NODES, RELATIONS]
+
+INDENT = '  '
