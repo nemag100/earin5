c8b6b48301c76b647a1d2b75fcea6f9c0b4ffe7e
nemag100 <jszwed23@gmail.com>
2021-05-06 19:56:19 +0200

    Added edges dictionary, improved validity check

--- bayes_net.py
+++ bayes_net.py
@@ -1,5 +1,7 @@
 import json
 
+from collections import defaultdict
+
 from constants import NODES, RELATIONS, PARENTS, PROBABILITIES, REQUIRED_KEYS
 from node import Node
 from utils import check_file, check_json, split_key, ConditionalProbability
@@ -9,16 +11,28 @@ class BayesNet:
 
     def __init__(self):
         self.nodes = {}
+        self.edges = defaultdict(list)
 
     def __str__(self):
         msg = ''
+        valid, _ = self.validate()
+        if not valid:
+            return msg
+        msg += 'Nodes:\n\n'
         for node in self.nodes.items():
             msg += '\"' + node[0] + '\" ' + str(node[1])
+        msg += '\nEdges:\n\n'
+        for edge in self.edges.items():
+            msg += str(edge) + '\n'
         return msg
 
     def load(self, filename):
         self.nodes = self._load_json(filename)
-        self.validate()
+        self._connect()
+        valid, err_msg = self.validate()
+        if not valid:
+            print('BayesNet invalid.')
+            print(err_msg)
 
     def mcmc(self, evidence={}, query=[]):
         """Returns probability estimates for each query,
@@ -33,19 +47,29 @@ class BayesNet:
         pass
 
     def validate(self):
+        msg = ''
         if not self.nodes:
-            print('No nodes initialized.')
-            return False
-        for node in self.nodes.values():
-            if not node.validate():
+            msg += 'No nodes initialized.'
+            return False, msg
+        for node in self.nodes.items():
+            valid, err_msg = node[1].validate()
+            if not valid:
                 self.nodes = {}
-                return False
-        return True
+                msg += 'Node \"' + node[0] + '\" invalid.'
+                msg += err_msg
+                return False, msg
+        return True, msg
 
     def check_cycles(self):
         """Evaluates to True if the bayesian network graph
         contains cycles, otherwise evaluates to False"""
         pass
+
+    def _connect(self):
+        """Updates edges dictionary."""
+        for node in self.nodes.items():
+            for parent in node[1].parents:
+                self.edges[parent].append(node[0])
 
     def _load_json(self, filename):
         """Loads json file into the dictionary of nodes."""
